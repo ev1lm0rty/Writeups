dde43868a280df27645ffb346a7a4bdfd91e2a85
Joker <jkr@localhost.localdomain>
2019-11-03 10:35:49 -0500

    First Commit

---
+++ HackTheBox/writeup/index.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html lang="en_US"><head>
+	<title>Home - writeup</title>
+	
+<base href="http://10.10.10.138/writeup/" />
+<meta name="Generator" content="CMS Made Simple - Copyright (C) 2004-2019. All rights reserved." />
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+	<!-- cms_stylesheet error: No stylesheets matched the criteria specified -->
+<style>.footer { background-color: white; position: fixed; left: 0; bottom: 0; width: 100%; color: black; text-align: center; }</style>
+</head><body>
+	<header id="header">
+		<h1>writeup</h1>
+	</header>
+
+	<nav id="menu">
+		
+
+
+
+
+<ul><li class="currentpage"><a class="currentpage" href="http://10.10.10.138/writeup/">Home Page</a></li><li><a href="http://10.10.10.138/writeup/index.php?page=ypuffy">ypuffy</a></li><li><a href="http://10.10.10.138/writeup/index.php?page=blue">blue</a></li><li><a href="http://10.10.10.138/writeup/index.php?page=writeup">writeup</a></li></ul>
+	</nav>
+
+	<section id="content">
+		<h1>Home</h1>
+		<p>After many month of lurking around on HTB I also decided to start writing about the boxes I hacked. In the upcoming days, weeks and month you will find more and more content here as I am about to convert my famous incomplete notes into pretty write-ups.</p>
+<p>I am still searching for someone to provide or make a cool theme. If you are interested, please contact me on&nbsp;<a href="https://mm.netsecfocus.com/">NetSec Focus Mattermost</a>. Thanks.</p>	</section>
+<div class="footer">
+  <p>Pages are hand-crafted with vim. NOT.</p>
+</div>
+
+</body>
+
+</html>
